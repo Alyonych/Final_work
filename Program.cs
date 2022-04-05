8aca66b1efe632bd51cdc4f4fa890c09dd2bb121
Alena Kuleshova <kuleshova.alena.m@gmail.com>
2022-04-05 14:55:35 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Из имеющегося массива строк сформировать массив из строк, длина которых меньше либо равна 3 символам. 
+// (Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении лучше обойтись массивами).
+
+string[] frstArry = {"program", "123", "bin", "final", "obj", "run", "p", "2", "ok"};
+string[] scndArry;
+int count = 0;
+int length = 0;
+
+    foreach (string elmnt in frstArry)
+        {
+        if (elmnt.Length <= 3)
+        length++;
+        }
+    scndArry = new string[length];
+        for (int j = 0; j < frstArry.Length; j++)
+            {
+            if (frstArry[j].Length <= 3)
+                {
+                scndArry[count] = frstArry[j];
+                Console.Write($"\t{count} ");
+                count++;   
+                }
+            }
+    Console.WriteLine();
+    foreach (string elmnt in scndArry)
+    {
+    Console.Write($"\t{elmnt} ");
+    }
+    Console.ReadLine();
